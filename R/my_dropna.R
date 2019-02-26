7c11d026cbee0721c10d6bf6ad011b2f60e66c28
Juno Chen <juercc@hotmail.com>
2019-02-26 15:38:56 -0800

    added code and updated tests for my_dropna

--- R/my_dropna.R
+++ R/my_dropna.R
@@ -7,5 +7,25 @@
 #'
 #' @examples my_dropna(input_dataframe)
 my_dropna <- function(input_dataframe) {
-
+  # check whether input is dataframe
+  if(is.data.frame(input_dataframe)==FALSE){
+    stop("Error: Expect input to be a dataframe")}
+  
+  rows <- nrow(input_dataframe)
+  columns <- ncol(input_dataframe)
+  Y<-input_dataframe
+  to_keep<-numeric(0)
+  
+  # check through each row
+  for(i in 1:rows) {
+    na_count <- 0
+    # check through each column
+    for(k in 1:columns) {
+      if(is.na(input_dataframe[i,k])){na_count <- na_count+1}}
+    # record index of all the rows with no NA
+    if(na_count==0){to_keep<-c(to_keep,i)}} 
+  # only keep the rows with no NA
+  Y <- Y[to_keep, ]
+  
+  return (Y)
 }
--- tests/testthat/test_my_dropna.R
+++ tests/testthat/test_my_dropna.R
@@ -1,3 +1,4 @@
+context("Testing my_dropna")
 #' February 2019
 #' This script tests the my_dropna function from the miniTidyR package
 #' 
@@ -5,20 +6,18 @@
 #' that containing `na` values removed
 #' 
 
-context("Testing my_dropna")
-
-#Test 1
+#Test 1: Testing that my_dropna prints an error when the first argument is not a dataframe
 
 test_that("In case input is not a dataframe, it throws an error", {
     
     vec <- c(0,1,2)
-    expect_error(my_dropna(vec), "Error: Expect the input to be a dataframe or a tibble")
+    expect_error(my_dropna(vec), "Error: Expect input to be a dataframe")
     
   })
 
-#Test 2
+#Test 2: Testing that my_dropna works on a dataframe containing NAs, and with data type as numbers and characters
 
-test_that("In case input is a dataframe containing numbers and characters, it works", {
+test_that("In case input is a dataframe containing numbers, characters, and NAs, it works", {
   
   df1 <- data.frame(A = c(12,25,NA,45), B = c(12,NA,30,45), C = c("Yes","No","Yes","No"))
   df2 <- data.frame(A = c(12,45), B = c(12,45), C = c("Yes","No"))
@@ -26,4 +25,11 @@ test_that("In case input is a dataframe containing numbers and characters, it wo
   expect_identical(my_dropna(df1), df2)
 })
 
+#Test 3: Testing that my_dropna works on a dataframe with no NAs
 
+test_that("In case input is a dataframe containing numbers, characters, and NAs, it works", {
+  
+  df3 <- data.frame(A = c(12,45), B = c(12,45), C = c("Yes","No"))
+  
+  expect_identical(my_dropna(df3), df3)
+})
